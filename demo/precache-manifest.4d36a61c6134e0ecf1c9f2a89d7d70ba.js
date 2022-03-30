d361ac7c8137b4c5efa5585f394b273a50459dce
Victor San Kho Lin <victor@sankholin.com>
2022-03-30 17:06:51 +1100

    Added DD visualizer setup for demo-ing model

---
+++ demo/precache-manifest.4d36a61c6134e0ecf1c9f2a89d7d70ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a52c359f2bbc3f2574f934ccec8d0813",
+    "url": "./index.html"
+  },
+  {
+    "revision": "62d75dd8ccd7c2234f65",
+    "url": "./static/css/2.e11889e3.chunk.css"
+  },
+  {
+    "revision": "f5c53404570f65eb0c34",
+    "url": "./static/css/main.f18def32.chunk.css"
+  },
+  {
+    "revision": "62d75dd8ccd7c2234f65",
+    "url": "./static/js/2.509b6363.chunk.js"
+  },
+  {
+    "revision": "493b37ccb92e234a58339a33ba7a6e5b",
+    "url": "./static/js/2.509b6363.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5c53404570f65eb0c34",
+    "url": "./static/js/main.45073f97.chunk.js"
+  },
+  {
+    "revision": "2962765925dae0082588",
+    "url": "./static/js/runtime-main.bc991cd5.js"
+  }
+]);
